7e4121795d5e73156a0f742b0bb0e7befb911969
LUCAS-PC\Lucas <lucaskchang@gmail.com>
2023-05-28 18:00:04 -0700

    title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'Ed-Nalysis'
+        }
+    },
     modules: [
         '@nuxtjs/tailwindcss',
         'nuxt-icon'
