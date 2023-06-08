012d4d0c4236cd120439f868c0d21e90bbe7c335
LUCAS-PC\Lucas <lucaskchang@gmail.com>
2023-06-07 19:50:23 -0700

    remove output folder

--- .output/public/_nuxt/BackButton.5746cc6a.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./nuxt-link.5b02e460.js";import{i as e,o as n,f as _,j as c,w as s,k as a}from"./entry.e2ac184e.js";const r={},i={class:"fixed bottom-4 left-4"},d=a("button",{class:"ring-2 ring-black py-2 px-4 rounded"}," Back ",-1);function f(m,p){const t=o;return n(),_("div",i,[c(t,{to:"/"},{default:s(()=>[d]),_:1})])}const u=e(r,[["render",f]]);export{u as _};
